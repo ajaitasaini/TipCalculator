dfc620e0ebf440d3b4b477089f02928b6fe05fbf
Ajaita Saini <ajaitasaini@fb.com>
2018-06-26 13:14:08 -0700

    Initial Commit

---
+++ TipCalculatorApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculatorApp
+//
+//  Created by Ajaita Saini on 6/26/18.
+//  Copyright © 2018 Ajaita Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
